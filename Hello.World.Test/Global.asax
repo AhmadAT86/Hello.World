930ec67c5332cfb4c687024e58208c8638fa9c7b
AhmadAT86 <ah.aboutaouk@gmail.com>
2016-01-26 11:42:41 +0100

    HelloWorld

---
+++ Hello.World.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello.World.Test.MvcApplication" Language="C#" %>
---
+++ Hello.World.Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello.World.Test.Startup))]
+namespace Hello.World.Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
